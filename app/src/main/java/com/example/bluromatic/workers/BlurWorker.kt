ba6825b46834a8611fa1c3034784509265aa1797
Joeltt <joelttm15@gmail.com>
2025-04-30 16:46:34 +0100

    feat: input and output image after blurring process

--- app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -2,10 +2,14 @@ package com.example.bluromatic.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -13,8 +17,12 @@ import kotlinx.coroutines.withContext
 
 private const val TAG = "BlurWorker"
 
-class BlurWorker(ctx: Context, params: WorkerParameters): CoroutineWorker(ctx, params) {
+class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
     override suspend fun doWork(): Result {
+
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
+
         makeStatusNotification(
             applicationContext.resources.getString(R.string.blurring_image),
             applicationContext
@@ -23,25 +31,48 @@ class BlurWorker(ctx: Context, params: WorkerParameters): CoroutineWorker(ctx, p
         //Create a withContext() block.
         //Inside the call to withContext() pass Dispatchers.IO so the lambda function runs in a special thread pool for potentially blocking IO operations.
         return withContext(Dispatchers.IO) {
-             try {
-                 // This is an utility function added to emulate slower work.
-                 delay(DELAY_TIME_MILLIS)
+            try {
+                /*
+                     Check that the resourceUri variable is populated.
+                     If it is not populated, your code should throw an exception.
+                     The code that follows is using the require() statement which throws an IllegalArgumentException
+                     if the first argument evaluates to false.
+                */
+                require(!resourceUri.isNullOrBlank()) {
+                    val errorMessage =
+                        applicationContext.resources.getString(R.string.invalid_input_uri)
+                    Log.e(TAG, errorMessage)
+                    errorMessage
+                }
 
-                val picture = BitmapFactory.decodeResource(
-                    applicationContext.resources,
-                    R.drawable.android_cupcake
+                /*
+                   Since the image source is passed in as a URI, we need a ContentResolver object to
+                   read the contents pointed to by the URI.
+                */
+                val resolver = applicationContext.contentResolver
+
+                // This is an utility function added to emulate slower work.
+                delay(DELAY_TIME_MILLIS)
+
+                /*
+                Because the image source is now the passed in URI, use BitmapFactory.decodeStream()
+                instead of BitmapFactory.decodeResource() to create the Bitmap object
+                 */
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
                 )
-                val output = blurBitmap(picture, 1)
+                val output = blurBitmap(picture, blurLevel)
+
                 // Write bitmap to a temp file
                 val outputUri = writeBitmapToFile(applicationContext, output)
-                //display a notification message to the user that contains the outputUri variable
-                makeStatusNotification(
-                    "Output is $outputUri",
-                    applicationContext
-                )
-                Result.success()
-            }catch (throwable: Throwable) {
-                Log.e(TAG,
+
+                val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+                Result.success(outputData)
+
+            } catch (throwable: Throwable) {
+                Log.e(
+                    TAG,
                     applicationContext.resources.getString(R.string.error_applying_blur),
                     throwable
                 )
