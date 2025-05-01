136b7b59b22ec1da85296ed9c187435e6d995219
Joeltt <joelttm15@gmail.com>
2025-05-01 11:14:40 +0100

    feat: ensure unique work

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,10 +19,12 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
+import com.example.bluromatic.IMAGE_MANIPULATION_WORK_NAME
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.getImageUri
@@ -44,8 +46,20 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
      * @param blurLevel The amount to blur the image
      */
     override fun applyBlur(blurLevel: Int) {
-        // Add WorkRequest to Cleanup temporary images
-        var continuation = workManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+//        // Add WorkRequest to Cleanup temporary images
+//        var continuation = workManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+        //you only want one chain of work to run at a time
+        var continuation = workManager
+            .beginUniqueWork(
+                IMAGE_MANIPULATION_WORK_NAME,
+                /*
+                This object tells the Android OS what happens if the work already exists.
+                you want to use REPLACE because if a user decides to blur another image before the current one finishes,
+                you want to stop the current one and start blurring the new image
+                */
+                ExistingWorkPolicy.REPLACE,
+                OneTimeWorkRequest.from(CleanupWorker::class.java)
+            )
         // Create WorkRequest to blur the image
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
         // New code for input data object
